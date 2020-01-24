528561415194ae140f6edd32220704ba864cc552
Javier Marín <javiermarindz@gmail.com>
2020-01-24 02:57:33 +0100

    index y css

---
+++ Practica 2 Enunciado/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gestion de Noticias. Página principal</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<p>Esta zona tiene el acceso restringido.<br>Para entrar debe identificarse</p>
+	<div id="caja">
+		<form action="gestion.php" method="post">
+			Usuario: <input type="text" name="txtUsuario"><br><br>
+			Clave: <input type="text" name="txtClave"><br><br>
+			<input type="submit" name="btnEntrar" value="Entrar">
+		</form>
+	</div>
+	<p>NOTA: Si no dispone de identificación o tiene problemas para entrar<br>póngase en contact con el <a href="#" title="administrador">administrador</a> del sitio</p>
+</body>
+</html>
+<?php
+
+
+
+
+
+?>
